48cdb62e9de567f392c2b4f9936e02597a2dbe94
Ghasemi Farinaz <farinaz.ghasemi@acast.com>
2024-03-17 15:24:56 +0100

    k8s has changed the repo

--- shells/cp-commands.sh
+++ shells/cp-commands.sh
@@ -1,3 +1,50 @@
+#latset version
+##########################################
+apt-get update && apt-get upgrade -y
+apt-get install -y vim
+apt install curl apt-transport-https vim git wget gnupg2 software-properties-common lsb-release ca-certificates uidmap -y
+swapoff -a
+modprobe overlay
+modprobe br_netfilter
+cat << EOF | tee /etc/sysctl.d/kubernetes.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+sysctl --system
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update && apt-get install containerd.io -y
+containerd config default | tee /etc/containerd/config.toml
+sed -e 's/SystemdCgroup = false/SystemdCgroup = true/g' -i /etc/containerd/config.toml
+systemctl restart containerd
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo apt-get update
+apt-get install -y kubeadm kubelet kubectl
+apt-mark hold kubelet kubeadm kubectl
+wget https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
+hostname -i
+ip addr show
+vim /etc/hosts
+vim kubeadm-config.yaml
+cat /etc/host
+cat /etc/hosts
+clear
+cat /etc/hosts
+ip addr show
+kubectl version --short
+kubectl version
+vim kubeadm-config.yaml
+cat kubeadm-config.yaml
+kubeadm init --config=kubeadm-config.yaml --upload-certs | tee kubeadm-init.out #<-- Save kubeadm-init.out for future review
+kubectl get pods
+exit
+history
+##########################################
+#old version
+
 sudo -i
 apt-get update && apt-get upgrade -y
 apt-get install -y vim
@@ -55,7 +102,7 @@ kind: ClusterConfiguration
 kubernetesVersion: 1.25.1 #<-- Use the word stable for newest version
 controlPlaneEndpoint: "k8scp:6443" #<-- Use the alias we put in /etc/hosts not the IP
 networking:
-  podSubnet: 192.168.0.0/16 #<-- Match the IP range from the Calico config file
+podSubnet: 192.168.0.0/16 #<-- Match the IP range from the Calico config file
 
 
 kubeadm init --config=kubeadm-config.yaml --upload-certs | tee kubeadm-init.out #<-- Save kubeadm-init.out for future review
