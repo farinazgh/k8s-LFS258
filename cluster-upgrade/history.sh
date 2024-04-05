56e47918d31c12af91af2f60c5eea4e998aec5d5
Ghasemi Farinaz <farinaz.ghasemi@acast.com>
2024-04-05 20:24:02 +0200

    adding some comments diff between kubeadm and kubelet/kubetcl

--- cluster-upgrade/history.sh
+++ cluster-upgrade/history.sh
@@ -1,8 +1,12 @@
+# vi /etc/apt/sources.list.d/kubernetes.list can be found here
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# this is not necessary if you remember/do  the above
+#curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 sudo apt update
 sudo apt-cache madison kubeadm
+
+# IMPORTANT this part should be done only for the api-server master the only node that has kubeadm
 
 sudo apt-mark unhold kubeadm && sudo apt-get update && sudo apt-get install -y kubeadm='1.29.3-1.1' && sudo apt-mark hold kubeadm
 kubeadm version
@@ -21,6 +25,7 @@ sudo apt-mark unhold kubelet kubectl && sudo apt-get update && sudo apt-get inst
 
 sudo -i
 
+# IMPORTANT this part should be done for all nodes in the cluster as all nodes have kubelet and kubectl
 sudo apt-mark unhold kubelet kubectl
 sudo apt-get update && sudo apt-get install -y kubelet='1.29.3-1.1' kubectl='1.29.3-1.1'
 sudo apt-mark hold kubelet kubectl
