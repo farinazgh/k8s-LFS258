c14cfb9ab6bc8490e54f63532d339972f5e042c7
Ghasemi Farinaz <farinaz.ghasemi@acast.com>
2024-04-07 16:12:58 +0200

    security context

---
+++ Labs/29-security-context.sh
@@ -0,0 +1,53 @@
+kubectl get pods
+kubectl get pod ubuntu-sleeper -o yaml
+kubectl describe  pod ubuntu-sleeper
+kubectl exec -it  ubuntu-sleeper -- whoami
+kubectl get securitycontext
+kubectl describe  pod ubuntu-sleeper | grep security
+kubectl get pod ubuntu-sleeper -o yaml | grep security
+kubectl get pod ubuntu-sleeper -o yaml > ubuntu-spec.yaml
+cat ubuntu-spec.yaml
+kubectl delete pod ubuntu-sleeper --force
+vi ubuntu-spec.yaml
+kubectl apply -f ubuntu-spec.yaml
+
+kubectl run ubuntu-sleeper --image=ubuntu --dry-run=client -o yaml > fromscratch.yaml
+vi fromscratch.yaml
+kubectl apply -f fromscratch.yaml
+
+
+
+#   apiVersion: v1
+#   kind: Pod
+#   metadata:
+#     name: ubuntu-sleeper
+#     namespace: default
+#   spec:
+#     containers:
+#     - command:
+#       - sleep
+#       - "4800"
+#       image: ubuntu
+#       name: ubuntu-sleeper
+#       securityContext:
+#         capabilities:
+#           add: ["SYS_TIME","NET_ADMIN"]
+#
+
+
+#apiVersion: v1
+#kind: Pod
+#metadata:
+#  name: ubuntu-sleeper
+#  namespace: default
+#spec:
+#  securityContext:
+#    runAsUser: 1010
+#  containers:
+#    - command:
+#        - sleep
+#        - "4800"
+#      image: ubuntu
+#      imagePullPolicy: Always
+#      name: ubuntu
+#      resources: {}
